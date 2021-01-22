b0976f04d54b39156cc40f62086bc6f5e34cb4ed
Elliott Vargo <vargoelliott@gmail.com>
2021-01-22 03:28:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,8 +29,9 @@ def monopoly_with_third_tier
   :pieces=>4,
   :names=>
   {
-    :reading_railrod=>{}
-
+    :reading_railrod=>{},
+    :pennsylvania_railroad=>{},
+    :b_and_o_railroad=>{},
   },
   :rent_in_dollars=>
   {:one_piece_owned=>25,
